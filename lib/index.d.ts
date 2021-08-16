e7d158f5f2632d1dcde59db48f803692cfae23fa
Ankush Girotra <ankush.girotra@gmail.com>
2021-08-16 02:34:55 -0500

    delete lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function get_altered_options<D extends {
-    [k: string]: any;
-}, P extends (Partial<D>)>(default_opts: D, partial_changes?: P): D;
-export declare function jcopy<T>(o: T): T;
-export declare function log(o: any): void;
-export declare const MakeID: (len?: number | undefined) => string;
-export declare function wait_ms(n: number): Promise<void>;
